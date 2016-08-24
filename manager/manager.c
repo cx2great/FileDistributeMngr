e35a3b7d28f2dc09ac773d231705573011a5590c
chenxi1 <chenxi1@xunlei.com>
2016-08-24 15:40:10 +0800

    complete diretory structure

--- manager/manager.c
+++ manager/manager.c
@@ -3,14 +3,19 @@
 #define data_port 6060
 #define ctrl_port 6070
 
-int Manger_init()
+int Manager_init()
 {
     
 }
 
+int Send2IPs(char *path)
+{
+
+}
+
 int main()
 {
-    Manger_init();
+    Manager_init();
     
     return 0 ;
 }
